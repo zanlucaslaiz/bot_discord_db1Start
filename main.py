5a2b0145db3fff3b1267bbd551e96010c42fa88f
zanlucaslaiz <zanlucaslaiz@gmail.com>
2022-10-01 16:32:19 -0300

    atualização

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+#bot para discord criado como projeto final do Bootcamp DB1 Start
+
+#bibliotecas usadas no projeto
 import discord
 import config
 import asyncio
 
-
+#iniciando o bot
 intents = discord.Intents.default()
 intents.message_content = True
 client =discord.Client(intents=intents)
 
+#eventos que o bot irá responder
 @client.event
 async def on_ready():
     print('online.')
